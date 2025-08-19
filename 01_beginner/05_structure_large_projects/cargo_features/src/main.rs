21aeaa4f13c4aa42e66378674e9433065f9cfb3c
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-08-19 10:47:40 -0700

    Learning about cargo features; enabling and disabling code not needed.

---
+++ 01_beginner/05_structure_large_projects/cargo_features/src/main.rs
@@ -0,0 +1,21 @@
+use draw::color;
+
+fn main() {
+    draw::draw_line(32, 32);
+
+    let color = color::RGB {
+        r: 247,
+        g: 76,
+        b: 0
+    };
+
+    color::draw_line(32, 32, &color);
+
+    let square = draw::shapes::Rectangle {
+        color,
+        width: 32,
+        height: 32
+    };
+
+    color::draw_line(32, 32, &square.color);
+}
