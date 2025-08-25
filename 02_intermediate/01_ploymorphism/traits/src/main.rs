d4172714ed92ca7584595d9b1fec19eeda6eaa51
ColorlessSaber <nuns-pastes-0s@icloud.com>
2025-08-25 09:35:57 -0700

    Learned about Trait Bounds.

--- 02_intermediate/01_ploymorphism/traits/src/main.rs
+++ 02_intermediate/01_ploymorphism/traits/src/main.rs
@@ -52,5 +52,41 @@ impl Paint for House {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let car = Car {
+        info: VehicleInfo {
+            make: "Honda".to_owned(),
+            model: "Civic".to_owned(),
+            year: 2015,
+        }
+    };
+
+    let house = House {};
+    let object = create_paintable_object();
+
+    paint_red(&car);
+    paint_red(&house);
+    paint_green(&object);
+
+    paint_vehicle_yellow(&car);
+    //paint_vehicle_yellow(&house); // error for house don't implement park trait
+    //paint_vehicle_yellow(&object); // error for object don't implement park trait
+}
+
+// Below are two ways to set trait bounds to an input
+fn paint_red<T: Paint>(object: &T) { // The T: Paint allows for any generic type that has the trait paint
+    object.paint("red".to_owned());
+}
+
+fn paint_green(object: &impl Paint) { // Using impl is syntax suger for the setup above.
+    object.paint("green".to_owned());
+}
+
+fn paint_vehicle_yellow<T>(object: &T) where T: Paint + Park { // T can be anything but must have the traits Paint and Park
+    object.paint("yellow".to_owned());
+}
+
+// traits can also be used as output. What is returned must implement that trait in question.
+// only works with one concrete type.
+fn create_paintable_object() -> impl Paint {
+    House {}
 }
